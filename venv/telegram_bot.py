613138d917b0e54a59963813402d08c46c87bcde
etherian <rianziwalker@gmail.com>
2025-01-04 21:41:45 +0700

    =

--- venv/telegram_bot.py
+++ venv/telegram_bot.py
@@ -17,3 +17,15 @@ CHAIN, WALLET, INTERVAL = range(3)
 user_data = {}
 
 # Create the function to get the information from insight api
+def fetch_transactions(
+        chain_id,
+        client_id,
+        wallet_address,
+        limit=1,
+        sort_by="block_number",
+        sort_order="desc",
+):
+    try: 
+    except requests.exceptions.RequestException as e:
+
+        return {"error": str(e)}
