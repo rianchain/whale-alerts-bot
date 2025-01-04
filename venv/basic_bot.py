51733d14b6f2676f1f990dbd89cba9d7530c02cf
etherian <rianziwalker@gmail.com>
2025-01-04 19:37:20 +0700

    define main func

--- venv/basic_bot.py
+++ venv/basic_bot.py
@@ -10,3 +10,12 @@ from telegram.ext import (
 # function when the user uses the /start command
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello to the new bot for whale alerts!")
+
+
+def main():
+    bot_token = "7618502843:AAGFj67PXpCmE18PpCvF86CnyRsa4u9JYOQ"
+
+    #create the application
+    application = Application.builder().token(bot_token).build()
+
+    # add handlers
